87bd51ba57051cfd1b21ba739ac406896f87c1ff
dangfan <dangf09@gmail.com>
2019-07-25 16:19:07 +0800

    ADD piv select command

--- piv/piv.c
+++ piv/piv.c
@@ -1,7 +1,26 @@
 #include <common.h>
 #include <piv.h>
 
-int piv_select(const CAPDU *capdu, RAPDU *rapdu) { return 0; }
+static const uint8_t rid[] = {0xA0, 0x00, 0x00, 0x03, 0x08};
+static const uint8_t pix[] = {0x00, 0x00, 0x10, 0x00, 0x01, 0x00};
+
+int piv_select(const CAPDU *capdu, RAPDU *rapdu) {
+  // This implementation is compatible with Yubikey 5, which is different from
+  // NIST SP 800-73-4
+  (void) capdu;
+  RDATA[0] = 0x61;
+  RDATA[1] = 6 + sizeof(pix) + sizeof(rid);
+  RDATA[2] = 0x4F;
+  RDATA[3] = sizeof(pix);
+  memcpy(RDATA + 4, pix, sizeof(pix));
+  RDATA[4 + sizeof(pix)] = 0x79;
+  RDATA[5 + sizeof(pix)] = 2 + sizeof(rid);
+  RDATA[6 + sizeof(pix)] = 0x4F;
+  RDATA[7 + sizeof(pix)] = sizeof(rid);
+  memcpy(RDATA + 8 + sizeof(pix), rid, sizeof(rid));
+  LL = 8 + sizeof(pix) + sizeof(rid);
+  return 0;
+}
 
 int piv_get_data(const CAPDU *capdu, RAPDU *rapdu) { return 0; }
 
