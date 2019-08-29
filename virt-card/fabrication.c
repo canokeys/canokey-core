31ebd831c884da70258356079dffd089249e0973
dangfan <dangf09@gmail.com>
2019-08-29 13:16:06 +0800

    FIX fabrication install error

--- virt-card/fabrication.c
+++ virt-card/fabrication.c
@@ -68,6 +68,7 @@ int u2f_fabrication_procedure() {
   lfs_emubd_create(&cfg, "lfs-root");
 
   fs_init(&cfg);
+  admin_install();
   u2f_config(16, aes128_enc, aes128_dec);
   fake_u2f_personalization();
 
