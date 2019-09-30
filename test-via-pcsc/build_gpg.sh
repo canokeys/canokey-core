edf0db40e375f201686fcbef4cc46150a9e40af0
z4yx <z4yx@users.noreply.github.com>
2019-10-01 00:42:10 +0800

    build latest GPG

---
+++ test-via-pcsc/build_gpg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+wget https://gnupg.org/ftp/gcrypt/pinentry/pinentry-1.1.0.tar.bz2
+tar -xf pinentry-1.1.0.tar.bz2
+pushd pinentry-1.1.0
+./configure --disable-pinentry-qt --enable-pinentry-tty
+make -j2
+sudo make install
+popd
+wget https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.2.17.tar.bz2
+tar -xf gnupg-2.2.17.tar.bz2
+pushd gnupg-2.2.17
+./configure --disable-doc --disable-wks-tools --disable-gpgtar --enable-gpg-is-gpg2 --disable-photo-viewers --disable-ldap
+make -j2
+sudo make install
+popd
