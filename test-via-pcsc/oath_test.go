cd1fdf92ebbb4ce76f8a84de363e6d4e8a72da9f
z4yx <z4yx@users.noreply.github.com>
2019-09-30 13:13:15 +0800

    more OATH tests

--- test-via-pcsc/oath_test.go
+++ test-via-pcsc/oath_test.go
@@ -14,6 +14,26 @@ import (
 	. "github.com/smartystreets/goconvey/convey"
 )
 
+func chooseAlgorithm() (alg1 ykoath.Algorithm, alg2 otp.Algorithm) {
+	if rand.Int()%2 == 0 {
+		alg1 = ykoath.HmacSha1
+		alg2 = otp.AlgorithmSHA1
+	} else {
+		alg1 = ykoath.HmacSha256
+		alg2 = otp.AlgorithmSHA256
+	}
+	return
+}
+
+func totpCodeShouldEqual(actual interface{}, expected ...interface{}) string {
+	if actual == expected[0] {
+		return ""
+	}
+	ts := expected[2].(time.Time)
+	return fmt.Sprintf("TOTP code %s should be %s\n(secret: %x)\n(time: %v)\n(algorithm: %v)",
+		expected[0], actual, expected[1], ts.Unix(), expected[3])
+}
+
 func TestOath(t *testing.T) {
 
 	Convey("OATH should work", t, func(ctx C) {
@@ -32,7 +52,7 @@ func TestOath(t *testing.T) {
 		_, err = oath.Select()
 		So(err, ShouldBeNil)
 
-		NumKeys := 10
+		NumKeys := 100
 
 		Convey("When name is too long", func(ctx C) {
 			name := strings.Repeat("O", 65)
@@ -65,26 +85,31 @@ func TestOath(t *testing.T) {
 			So(err.Error(), ShouldEqual, "no such object")
 		})
 
-		Convey("By adding some keys", func(ctx C) {
+		Convey("Firstly add several keys", func(ctx C) {
 
 			allKeys := make(map[string]*otp.Key)
+			key2Alg := make(map[string]otp.Algorithm)
 			for i := 0; i < NumKeys; i++ {
-				name := fmt.Sprintf("name%060d", i)
+				alg1, alg2 := chooseAlgorithm()
+
+				name := fmt.Sprintf("Index%054dHmac%d", i, alg1)
 				key := make([]byte, 64)
 				_, err := crand.Read(key)
 				So(err, ShouldBeNil)
 
 				// fmt.Printf("adding key %d %s %v\n", i, name, hex.EncodeToString(key))
-				err = oath.Put(name, ykoath.HmacSha1, ykoath.Totp, 6, key, false)
+				err = oath.Put(name, alg1, ykoath.Totp, 6, key, false)
 				So(err, ShouldBeNil)
 
 				opts := totp.GenerateOpts{
 					Secret:      key,
 					Issuer:      "Someone",
 					AccountName: name,
+					Algorithm:   alg2,
 				}
 				allKeys[name], err = totp.Generate(opts)
 				So(err, ShouldBeNil)
+				key2Alg[name] = alg2
 			}
 
 			Reset(func() {
@@ -92,6 +117,10 @@ func TestOath(t *testing.T) {
 					err := oath.Delete(name)
 					So(err, ShouldBeNil)
 				}
+
+				lResult, err := oath.List()
+				So(err, ShouldBeNil)
+				So(lResult, ShouldBeEmpty)
 			})
 
 			Convey("Then list keys", func(ctx C) {
@@ -117,9 +146,14 @@ func TestOath(t *testing.T) {
 					So(err, ShouldBeNil)
 					// fmt.Printf("%s %s\n", otp, name)
 
-					otpHost, err := totp.GenerateCode(allKeys[name].Secret(), oath.Clock())
+					otpHost, err := totp.GenerateCodeCustom(allKeys[name].Secret(), oath.Clock(), totp.ValidateOpts{
+						Period:    30,
+						Skew:      1,
+						Digits:    6,
+						Algorithm: key2Alg[name],
+					})
 					So(err, ShouldBeNil)
-					So(otp, ShouldEqual, otpHost)
+					So(otpHost, totpCodeShouldEqual, otp, allKeys[name].Secret(), oath.Clock(), key2Alg[name].String())
 				}
 			})
 
@@ -130,9 +164,14 @@ func TestOath(t *testing.T) {
 					So(allKeys, ShouldContainKey, name)
 					// fmt.Printf("%s %s\n", otp, name)
 
-					otpHost, err := totp.GenerateCode(allKeys[name].Secret(), oath.Clock())
+					otpHost, err := totp.GenerateCodeCustom(allKeys[name].Secret(), oath.Clock(), totp.ValidateOpts{
+						Period:    30,
+						Skew:      1,
+						Digits:    6,
+						Algorithm: key2Alg[name],
+					})
 					So(err, ShouldBeNil)
-					So(otp, ShouldEqual, otpHost)
+					So(otpHost, totpCodeShouldEqual, otp, allKeys[name].Secret(), oath.Clock(), key2Alg[name].String())
 				}
 			})
 
