c7a627e3d2058011de079f067de9416abe1d3644
z4yx <z4yx@users.noreply.github.com>
2019-10-01 14:00:55 +0800

    patch pinentry to read password from files

--- test-via-pcsc/build_gpg.sh
+++ test-via-pcsc/build_gpg.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
+mkdir gnupg
+pushd gnupg
 wget https://gnupg.org/ftp/gcrypt/pinentry/pinentry-1.1.0.tar.bz2
 tar -xf pinentry-1.1.0.tar.bz2
+patch -p1 <../test-via-pcsc/pinentry-mock.patch
 pushd pinentry-1.1.0
-./configure --disable-pinentry-qt --enable-pinentry-tty
+./configure --disable-pinentry-qt --enable-pinentry-tty --disable-pinentry-curses --disable-pinentry-gtk2
 make -j2
 sudo make install
 popd
@@ -13,4 +16,5 @@ pushd gnupg-2.2.17
 ./configure --disable-doc --disable-wks-tools --disable-gpgtar --enable-gpg-is-gpg2 --disable-photo-viewers --disable-ldap
 make -j2
 sudo make install
+popd
 popd
