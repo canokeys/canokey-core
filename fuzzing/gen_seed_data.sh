24f16d775b966acaf1209e7603081458089bb18c
z4yx <z4yx@users.noreply.github.com>
2019-10-26 18:48:27 +0800

    add initial data for fuzzing test

---
+++ fuzzing/gen_seed_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+FILE_IN="$1"
+file_index=1
+
+while read -r line; do 
+    grep -Po '( \w\w)+ *$' <<<$line |xxd -r -p >"${FILE_IN%txt}$file_index.txt"
+    ((file_index++))
+done < "$FILE_IN"
