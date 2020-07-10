081202973649c47c8831b679326b090f16c95255
z4yx <z4yx@users.noreply.github.com>
2020-07-10 16:10:58 +0800

    reorder 2 options in GetInfo

--- applets/ctap/ctap.c
+++ applets/ctap/ctap.c
@@ -605,13 +605,13 @@ static uint8_t ctap_get_info(CborEncoder *encoder) {
   CborEncoder option_map;
   ret = cbor_encoder_create_map(&map, &option_map, 2);
   CHECK_CBOR_RET(ret);
-  ret = cbor_encode_text_stringz(&option_map, "clientPin");
-  CHECK_CBOR_RET(ret);
-  ret = cbor_encode_boolean(&option_map, has_pin() > 0);
-  CHECK_CBOR_RET(ret);
   ret = cbor_encode_text_stringz(&option_map, "rk");
   CHECK_CBOR_RET(ret);
   ret = cbor_encode_boolean(&option_map, true);
+  CHECK_CBOR_RET(ret);
+  ret = cbor_encode_text_stringz(&option_map, "clientPin");
+  CHECK_CBOR_RET(ret);
+  ret = cbor_encode_boolean(&option_map, has_pin() > 0);
   CHECK_CBOR_RET(ret);
   ret = cbor_encoder_close_container(&map, &option_map);
   CHECK_CBOR_RET(ret);
