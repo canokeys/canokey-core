4aa599ad514222229464e35c88e6377a37e36f9b
Zenithal <i@zenithal.me>
2020-12-08 03:39:58 +0800

    NDEF: implement CC type and default value

--- applets/ndef/ndef.c
+++ applets/ndef/ndef.c
@@ -3,10 +3,42 @@
 #include <memzero.h>
 #include <udef.h>
 
+#define CC_FILE "E103" // file identifier also 0xE103
+#define NDEF_FILE "NDEF"
+
+static const ndef_cc_t init_cc = {
+.len = 0x000F,
+.ver = 0x20,// 2.0
+.mle = 0x000F, // default now, hareware info needed
+.mlc = 0x0001, // default now, hardware info needed
+.tlv = {
+  .t = 0x04,
+  .l = 0x06,
+  .id = 0x0001, // not determined now
+  .max_size = 0xFFFE, // not determined now
+  .r = 0x00, // read access without any security
+  .w = 0x00 // write access without any security
+  }
+};
+
+static const ndef_cc_t current_cc;
+
 void ndef_poweroff(void) {
 }
 
+int ndef_create_empty_ndef() {
+  return 0;
+}
+
 int ndef_install(uint8_t reset) {
+  if (reset || get_file_size(CC_FILE) != sizeof(ndef_cc_t)
+            || get_file_size(NDEF_FILE) <= 0) {
+    current_cc = init_cc;
+    if (write_file(CC_FILE, &current_cc, 0, sizeof(current_cc), 1) < 0) return -1;
+    if (ndef_create_empty_ndef() < 0) return -1;
+  } else {
+    if (read_file(CC_FILE, &current_cc, 0, sizeof(current_cc)) < 0) return -1;
+  }
   return 0;
 }
 
--- include/ndef.h
+++ include/ndef.h
@@ -4,6 +4,24 @@
 
 #include <apdu.h>
 
+typedef struct {
+  uint8_t t;
+  uint8_t l;
+  // the following are v of tlv
+  uint16_t id;
+  uint16_t max_size;
+  uint8_t r;
+  uint8_t w;
+} ndef_tlv_t;
+
+typedef struct {
+  uint16_t len;
+  uint8_t ver;
+  uint16_t mle;
+  uint16_t mlc;
+  ndef_tlv_t tlv;
+} ndef_cc_t;
+
 void ndef_poweroff(void);
 int ndef_install(uint8_t reset);
 int ndef_process_apdu(const CAPDU *capdu, RAPDU *rapdu);
