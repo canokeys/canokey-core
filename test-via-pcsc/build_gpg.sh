d9a5b20894337d28ff99c10fb9210c4a99bd91d6
z4yx <z4yx@users.noreply.github.com>
2021-10-29 21:30:42 +0800

    upgrade GPG for testing
    fix build of GPG

--- test-via-pcsc/build_gpg.sh
+++ test-via-pcsc/build_gpg.sh
@@ -4,6 +4,34 @@ mkdir ~/.gnupg || true
 echo "pinentry-program /usr/local/bin/pinentry-tty" >~/.gnupg/gpg-agent.conf
 mkdir gnupg || true
 pushd gnupg
+# Remove old versions on system
+sudo rm /usr/lib/x86_64-linux-gnu/pkgconfig/gpg-error.pc
+sudo rm /usr/lib/x86_64-linux-gnu/pkgconfig/libgcrypt.pc
+sudo rm /usr/include/x86_64-linux-gnu/gpg*
+sudo rm /usr/lib/x86_64-linux-gnu/libgpg-error.*
+sudo rm /usr/lib/x86_64-linux-gnu/libgcrypt.*
+if [ ! -d libgpg-error-1.42 ];then
+    wget https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.42.tar.bz2
+    tar -xf libgpg-error-1.42.tar.bz2
+    pushd libgpg-error-1.42
+    ./configure --prefix=/usr
+    make -j2
+else
+    pushd libgpg-error-1.42
+fi
+sudo make install
+popd
+if [ ! -d libgcrypt-1.9.4 ];then
+    wget https://gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.9.4.tar.bz2
+    tar -xf libgcrypt-1.9.4.tar.bz2
+    pushd libgcrypt-1.9.4
+    ./configure --prefix=/usr
+    make -j2
+else
+    pushd libgcrypt-1.9.4
+fi
+sudo make install
+popd
 if [ ! -d pinentry-1.1.0 ];then
     wget https://gnupg.org/ftp/gcrypt/pinentry/pinentry-1.1.0.tar.bz2
     tar -xf pinentry-1.1.0.tar.bz2
@@ -16,14 +44,14 @@ else
 fi
 sudo make install
 popd
-if [ ! -d gnupg-2.2.17 ];then
-    wget https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.2.17.tar.bz2
-    tar -xf gnupg-2.2.17.tar.bz2
-    pushd gnupg-2.2.17
+if [ ! -d gnupg-2.3.3 ];then
+    wget https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.3.3.tar.bz2
+    tar -xf gnupg-2.3.3.tar.bz2
+    pushd gnupg-2.3.3
     ./configure --prefix=/usr --disable-doc --disable-wks-tools --disable-gpgtar --disable-photo-viewers --disable-ldap
     make -j2
 else
-    pushd gnupg-2.2.17
+    pushd gnupg-2.3.3
 fi
 sudo make install
 popd
