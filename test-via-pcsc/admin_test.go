66a650cdf374a975b6e855160567bc60732250df
dangfan <dangf09@gmail.com>
2021-12-01 09:40:01 +0800

    change the reader name

--- test-via-pcsc/admin_test.go
+++ test-via-pcsc/admin_test.go
@@ -41,6 +41,7 @@ func New() (*AdminApplet, error) {
 	for _, reader := range readers {
 		// fmt.Printf("Reader: %s\n", reader)
 		if strings.Contains(reader, "Canokey") && strings.Contains(reader, "OATH") {
+		if strings.Contains(strings.ToLower(reader), "canokey") && strings.Contains(reader, "OATH") {
 			card, err := context.Connect(reader, scard.ShareShared, scard.ProtocolAny)
 			if err != nil {
 				context.Release()
