bf71533081af4e7d4fe2a9f0dfe4dd16631aac27
z4yx <z4yx@users.noreply.github.com>
2021-12-07 22:46:50 +0800

    USB sending timeout

--- interfaces/USB/class/ctaphid/usbd_ctaphid.c
+++ interfaces/USB/class/ctaphid/usbd_ctaphid.c
@@ -102,7 +102,6 @@ uint8_t USBD_CTAPHID_DataOut(USBD_HandleTypeDef *pdev) {
 
 uint8_t USBD_CTAPHID_SendReport(USBD_HandleTypeDef *pdev, uint8_t *report, uint16_t len) {
   if (pdev->dev_state == USBD_STATE_CONFIGURED) {
-#ifndef TEST
     volatile CTAPHID_StateTypeDef *state = &hid_handle.state;
     int retry = 0;
     while (*state != CTAPHID_IDLE) {
@@ -110,7 +109,6 @@ uint8_t USBD_CTAPHID_SendReport(USBD_HandleTypeDef *pdev, uint8_t *report, uint1
       if (++retry > 50) return USBD_BUSY;
       device_delay(1);
     }
-#endif
     hid_handle.state = CTAPHID_BUSY;
     USBD_LL_Transmit(pdev, EP_IN(ctap_hid), report, len);
   }
