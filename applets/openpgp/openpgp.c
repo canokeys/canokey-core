d77fe56fa85f84fa505736ffe8ca04b8ecd91274
ZenithalHourlyRate <i@zenithal.me>
2022-02-17 01:23:16 +0800

    Fix Wimplicit-fallthrough

--- applets/openpgp/openpgp.c
+++ applets/openpgp/openpgp.c
@@ -853,6 +853,7 @@ static int openpgp_generate_asymmetric_key_pair(const CAPDU *capdu, RAPDU *rapdu
     case X25519:
       // swap endianness only for x25519
       swap_big_number_endian(key + KEY_SIZE_25519);
+      __attribute__ ((fallthrough));
     case ED25519:
       RDATA[2] = KEY_SIZE_25519 + 2;
       RDATA[3] = 0x86;
