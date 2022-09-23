1403b1e8a294f76770c68160e2b4290a0a4e2640
z4yx <z4yx@users.noreply.github.com>
2022-09-23 23:34:55 +0800

    build the latest version of libfido2

--- test-via-pcsc/build_fido_tests.sh
+++ test-via-pcsc/build_fido_tests.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 set -e
+if [ ! -d u2f-ref-code ];then
 git clone --depth 1 https://github.com/google/u2f-ref-code.git
 pushd u2f-ref-code/u2f-tests/HID
 git clone --depth 1 -b lollipop-release https://android.googlesource.com/platform/system/core
 cd ../NFC; make
 cd ../HID; make
 popd
+fi
 
 git clone --depth 1 https://github.com/canokeys/fido2-tests.git
 pushd fido2-tests
 pip3 install --user -r requirements.txt
+popd
+
+if [ ! -d libfido2 ];then
+git clone --depth 1 https://github.com/Yubico/libfido2.git
+mkdir libfido2/build
+pushd libfido2/build
+cmake -DUSE_PCSC=ON ..
+make -j2
+else
+pushd libfido2/build
+fi
+sudo make install
 popd
