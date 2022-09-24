43cd7a51e79e01e8be7d1c5546fdd8e2c197c07d
z4yx <z4yx@users.noreply.github.com>
2022-09-25 00:04:36 +0800

    test RK deletion

--- test-real/test-libfido2.sh
+++ test-real/test-libfido2.sh
@@ -17,13 +17,13 @@ oneTimeSetUp() {
 ToolHelper() {
     # echo "PIN is $PIN"
     echo $PIN | $NON_TTY $* "$RDID" 2>"$TEST_TMP_DIR/stderr"
-    grep -v "Enter PIN " "$TEST_TMP_DIR/stderr" 1>&2
+    grep -v "Enter PIN " "$TEST_TMP_DIR/stderr" 1>&2 || true
 }
 
 FIDO2MakeCred() {
     rpid=$1
     username=$2
-    userid=$(openssl rand -base64 64)
+    userid=$(dd status=none if=/dev/urandom bs=64 count=1 | base64 -w 0)
     openssl rand -base64 32 >"$TEST_TMP_DIR/cred_param" # client data hash
     echo "$rpid" >>"$TEST_TMP_DIR/cred_param"
     echo "$username" >>"$TEST_TMP_DIR/cred_param"
@@ -35,22 +35,31 @@ FIDO2MakeCred() {
 
 FIDO2ListRP() {
     nrRk=$(ToolHelper fido2-token -I -c | grep 'existing rk')
-    echo "$nrRk"
+    echo "$nrRk" # existing rk(s): 64
     if [[ $nrRk != *" 0" ]]; then
+        # Idx Credential                                   RpID
+        # 00: NBuC7jAK2Ty/3ileQvETIZ8BUQ+93GoraEm3Su3KvC0= RPID_aaaaaaaaaaaaabbbbbbbbbbbb01
         ToolHelper fido2-token -L -r
     fi
 }
 
 FIDO2SetName() {
-    cred_id="$1"
-    user_id="$2"
+    credid="$1"
+    userid="$2"
     name="$3"
     display_name="$4"
-    ToolHelper fido2-token -S -c -i "$cred_id" -k "$user_id" -n "$name" -p "$display_name"
+    echo "Set Cred=$credid User=$userid to $name $display_name"
+    ToolHelper fido2-token -S -c -i "$credid" -k "$userid" -n "$name" -p "$display_name"
 }
 
 FIDO2GetRkByRp() {
+    # Idx: CredID DispName UserID Algo Prot
+    # 00: +CSO/Hjxmj8YJ0Iv+TQw018+a+8y+AE36XlODTT6vhUBADQbgu4wCtk8v94pXkLxEyGfAVEPvdxqK2hJt0rtyrwtd+Az/H8AABB24DP8fwAA/7CVQft/AAAAAAAAAAAAACB1+f///w== DispName E+It8WJq4TJbPzfjSqeJDPpP+XkKVMBzIAk0sKAVu8IaZDhG2vOEH4rqw0eP6yWg es256 unknown
     ToolHelper fido2-token -L -k "$1"
+}
+
+FIDO2DelRkByID() {
+    ToolHelper fido2-token -D -i "$1" -n "$2"
 }
 
 makeRPID() {
@@ -61,6 +70,13 @@ makeRPID() {
 makeUserName() {
     # USER_NAME_LIMIT=65
     printf "USERNAME_aaaaaaaaaaaaabbbbbbbbbbbb_ccccccccccccccdddddddddddddd%02x" $1
+}
+
+makeCredAndStore() {
+    fields=($(FIDO2MakeCred $1 $2))
+    userid=${fields[0]}
+    credid=${fields[1]}
+    echo $1 $userid $2 $credid | tee -a "$TEST_TMP_DIR/rks"
 }
 
 test_Reset() {
@@ -81,10 +97,7 @@ test_MC() {
     for((i=1;i<=64;i++)); do
         rpid=$(makeRPID $i)
         uname=$(makeUserName $i)
-        fields=($(FIDO2MakeCred $rpid $uname))
-        userid=${fields[0]}
-        credid=${fields[1]}
-        echo $rpid $userid $uname $credid | tee -a "$TEST_TMP_DIR/rks"
+        makeCredAndStore "$rpid" "$uname"
     done
     nline=0
     while IFS= read -r line
@@ -101,19 +114,36 @@ test_MC() {
 }
 
 test_DispName() {
-    for nline in 1 ;do #32 64
-        golden=($(sed -ne "${line}p" "$TEST_TMP_DIR/rks"))
-        rpid=${golden[0]}
-        userid=${golden[1]}
-        credid=${golden[3]}
+    for i in 1 ;do #32 64
+        rpid=$(makeRPID $i)
+        fields=($(grep $rpid "$TEST_TMP_DIR/rks"))
+        userid=${fields[1]}
+        credid=${fields[3]}
         display_name=DISP_NAME
-        FIDO2SetName "$credid" "$userid" myasdf "$display_name"
+        FIDO2SetName "$credid" "$userid" "new_username" "$display_name"
         fields=($(FIDO2GetRkByRp $rpid))
         assertEquals "$display_name" "${fields[2]}"
         assertEquals "$userid"       "${fields[3]}"
         assertEquals es256           "${fields[4]}"
     done
 
+}
+
+test_DelRk() {
+    randSeq=$(seq 1 64 | shuf)
+    for i in $randSeq; do
+        rpid=$(makeRPID $i)
+        fields=($(grep $rpid "$TEST_TMP_DIR/rks"))
+        credid=${fields[3]}
+        FIDO2DelRkByID $credid $rpid
+        sed -i "/$rpid/d" "$TEST_TMP_DIR/rks"
+    done
+}
+
+test_Debug() {
+    # FIDO2MakeCred rp1 un2
+    # FIDO2GetRkByRp RPID_aaaaaaaaaaaaabbbbbbbbbbbb01
+    # FIDO2DelRkByID "6AwF68LTVupyLx5ddpFRQiPS9+UmkSktTXYWREijOjIBAGO0QIKafRKTv8hiGj4aZxPQSQbfySYyH7CGSbLfBM8/d+Az/H8AABB24DP8fwAA/7CVQft/AAAAAAAAAAAAACB1+f///w==" RPID_aaaaaaaaaaaaabbbbbbbbbbbb40
 }
 
 . ./shunit2/shunit2
