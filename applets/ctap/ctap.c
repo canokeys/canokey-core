f7bb585be90f17432301cd2f79f596adf0102c12
Fan DANG <i@dang.fan>
2022-11-09 20:25:08 +0800

    refactor fido

--- applets/ctap/ctap.c
+++ applets/ctap/ctap.c
@@ -226,14 +226,14 @@ static uint8_t ctap_make_credential(CborEncoder *encoder, uint8_t *params, size_
   uint8_t data_buf[sizeof(CTAP_authData)];
   if (mc.excludeListSize > 0) {
     for (size_t i = 0; i < mc.excludeListSize; ++i) {
-      uint8_t pri_key[PRI_KEY_SIZE];
+      ecc_key_t key;
       parse_credential_descriptor(&mc.excludeList, data_buf); // save credential id in data_buf
       CredentialId *kh = (CredentialId *)data_buf;
       // compare rpId first
       if (memcmp(kh->rpIdHash, mc.rpIdHash, sizeof(kh->rpIdHash)) != 0) continue;
       // then verify key handle and get private key in rpIdHash
-      ret = verify_key_handle(kh, pri_key);
-      memzero(pri_key, sizeof(pri_key));
+      ret = verify_key_handle(kh, &key);
+      memzero(&key, sizeof(key));
       if (ret < 0) return CTAP2_ERR_UNHANDLED_REQUEST;
       if (ret == 0) {
         DBG_MSG("Exclude ID found\n");
@@ -328,7 +328,7 @@ static uint8_t ctap_make_credential(CborEncoder *encoder, uint8_t *params, size_
     sha256_update(data_buf, len);
     sha256_update(mc.clientDataHash, sizeof(mc.clientDataHash));
     sha256_final(data_buf);
-    len = sign_with_device_key(data_buf, data_buf);
+    len = sign_with_device_key(data_buf, PRIVATE_KEY_LENGTH[SECP256R1], data_buf);
     ret = cbor_encode_byte_string(&att_map, data_buf, len);
     CHECK_CBOR_RET(ret);
 
@@ -388,7 +388,8 @@ static uint8_t ctap_get_assertion(CborEncoder *encoder, uint8_t *params, size_t 
 #endif
   }
 
-  uint8_t data_buf[sizeof(CTAP_authData) + CLIENT_DATA_HASH_SIZE], pri_key[PRI_KEY_SIZE];
+  uint8_t data_buf[sizeof(CTAP_authData) + CLIENT_DATA_HASH_SIZE];
+  ecc_key_t key;  // TODO: cleanup
   CTAP_residentKey rk;
   if (ga.allowListSize > 0) {
     size_t i;
@@ -397,7 +398,7 @@ static uint8_t ctap_get_assertion(CborEncoder *encoder, uint8_t *params, size_t 
       // compare rpId first
       if (memcmp(rk.credential_id.rpIdHash, ga.rpIdHash, sizeof(rk.credential_id.rpIdHash)) != 0) goto next;
       // then verify key handle and get private key
-      int err = verify_key_handle(&rk.credential_id, pri_key);
+      int err = verify_key_handle(&rk.credential_id, &key);
       if (err < 0) return CTAP2_ERR_UNHANDLED_REQUEST;
       if (err == 0) break; // only process one support credential
     next:
@@ -433,7 +434,7 @@ static uint8_t ctap_get_assertion(CborEncoder *encoder, uint8_t *params, size_t 
     size =
         read_file(RK_FILE, &rk, credential_list[credential_idx] * sizeof(CTAP_residentKey), sizeof(CTAP_residentKey));
     if (size < 0) return CTAP2_ERR_UNHANDLED_REQUEST;
-    int err = verify_key_handle(&rk.credential_id, pri_key);
+    int err = verify_key_handle(&rk.credential_id, &key);
     if (err != 0) return CTAP2_ERR_UNHANDLED_REQUEST;
   }
 
@@ -524,16 +525,8 @@ static uint8_t ctap_get_assertion(CborEncoder *encoder, uint8_t *params, size_t 
   // signature
   ret = cbor_encode_int(&map, RESP_signature);
   CHECK_CBOR_RET(ret);
-  if (rk.credential_id.alg_type == COSE_ALG_ES256) {
-    sha256_init();
-    sha256_update(data_buf, len);
-    sha256_update(ga.clientDataHash, sizeof(ga.clientDataHash));
-    sha256_final(data_buf);
-    len = sign_with_ecdsa_private_key(pri_key, data_buf, data_buf);
-  } else if (rk.credential_id.alg_type == COSE_ALG_EDDSA) {
-    memcpy(data_buf + len, ga.clientDataHash, CLIENT_DATA_HASH_SIZE);
-    len = sign_with_ed25519_private_key(pri_key, data_buf, len + CLIENT_DATA_HASH_SIZE, data_buf);
-  }
+  memcpy(data_buf + len, ga.clientDataHash, CLIENT_DATA_HASH_SIZE);
+  len = sign_with_private_key(rk.credential_id.alg_type, &key, data_buf, len + CLIENT_DATA_HASH_SIZE, data_buf);
   ret = cbor_encode_byte_string(&map, data_buf, len);
   CHECK_CBOR_RET(ret);
 
@@ -578,7 +571,6 @@ static uint8_t ctap_get_assertion(CborEncoder *encoder, uint8_t *params, size_t 
   ret = cbor_encoder_close_container(encoder, &map);
   CHECK_CBOR_RET(ret);
 
-  memzero(pri_key, sizeof(pri_key));
   ++credential_idx;
 
   return 0;
