96fe3fd5f329416bb9784586ce06d3e9e5773cf6
z4yx <z4yx@users.noreply.github.com>
2023-08-22 08:10:13 +0800

    Fixing a bug in python-fido2 0.9.3

--- test-via-pcsc/build_fido_tests.sh
+++ test-via-pcsc/build_fido_tests.sh
@@ -12,6 +12,20 @@ fi
 git clone --depth 1 -b dev-fido2v1 https://github.com/canokeys/fido2-tests.git
 pushd fido2-tests
 pip3 install --user -r requirements.txt
+echo "Fixing a bug in python-fido2 0.9.3"
+patch -p1 -u -d ~/.local/lib/python3.*/site-packages/fido2 <<EOF
+--- fido2/ctap2/blob.py 2023-08-22 21:09:59.905129124 +0800
++++ fido2.fix/ctap2/blob.py  2023-08-22 21:14:07.014840263 +0800
+@@ -150,7 +150,7 @@
+             self.ctap.large_blobs(
+                 offset,
+                 set=_set,
+-                length=ln,
++                length=(size if offset == 0 else None),
+                 pin_uv_protocol=pin_uv_protocol,
+                 pin_uv_param=pin_uv_param,
+             )
+EOF
 popd
 
 if [ ! -d libfido2 ];then
