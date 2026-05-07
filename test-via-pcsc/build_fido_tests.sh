e2428fa3e28b343017a2cd82ad792ba8e4bbb242
Fan DANG <i@dang.fan>
2026-05-07 12:44:49 +0800

    Update FIDO2 tests pin for SM2 -54

--- test-via-pcsc/build_fido_tests.sh
+++ test-via-pcsc/build_fido_tests.sh
@@ -2,7 +2,7 @@
 # Build FIDO test tools (only runs on cache miss)
 set -e
 
-readonly FIDO2_TESTS_REF=4cd674500f82a0f050c29ef334d8e35a83233932
+readonly FIDO2_TESTS_REF=de017b7e5359eff7b169485bff757a42717644c4
 
 if [ ! -d u2f-ref-code ]; then
   git clone --depth 1 https://github.com/google/u2f-ref-code.git
