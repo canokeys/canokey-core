9e87c0962a20784d128ccd633bae207631678557
Fan DANG <i@dang.fan>
2026-08-15 17:37:22 +0800

    Use unified AES interface for CTAP PIN crypto

--- applets/ctap/secret.c
+++ applets/ctap/secret.c
@@ -2,7 +2,6 @@
 #include "cose-key.h"
 #include "secret.h"
 #include <aes.h>
-#include <block-cipher.h>
 #include <crypto-util.h>
 #include <ecc.h>
 #include <fs.h>
@@ -123,19 +122,16 @@ int cp_decapsulate(uint8_t *buf, int pin_protocol) {
 
 int cp_encrypt(const uint8_t *key, const uint8_t *in, size_t in_size, uint8_t *out, int pin_protocol) {
   uint8_t iv[16];
-  block_cipher_config cfg = {.block_size = 16, .mode = CBC, .iv = iv, .encrypt = aes256_enc, .decrypt = aes256_dec};
-  cfg.in_size = in_size;
-  cfg.in = in;
-  cfg.out = out;
+  const uint8_t *aes_key;
   if (pin_protocol == 1) {
     memzero(iv, sizeof(iv));
-    cfg.key = key;
+    aes_key = key;
   } else {
     random_buffer(iv, sizeof(iv));
-    cfg.key = key + SHARED_SECRET_SIZE_HMAC;
+    aes_key = key + SHARED_SECRET_SIZE_HMAC;
   }
-  int ret = block_cipher_enc(&cfg);
-  if (pin_protocol == 2) {
+  int ret = aes_crypt(AES_MODE_CBC, AES_OP_ENCRYPT, AES_KEY_256, aes_key, iv, in, out, in_size);
+  if (ret == 0 && pin_protocol == 2) {
     // "in" and "out" arguments can be the same pointer
     memmove(out + sizeof(iv), out, in_size);
     memcpy(out, iv, sizeof(iv));
@@ -149,21 +145,21 @@ int cp_encrypt_pin_token(const uint8_t *key, uint8_t *out, int pin_protocol) {
 
 int cp_decrypt(const uint8_t *key, const uint8_t *in, size_t in_size, uint8_t *out, int pin_protocol) {
   uint8_t iv[16];
-  block_cipher_config cfg = {.block_size = 16, .mode = CBC, .iv = iv, .encrypt = aes256_enc, .decrypt = aes256_dec};
-  cfg.out = out;
   if (pin_protocol == 1) {
     memzero(iv, sizeof(iv));
-    cfg.key = key;
-    cfg.in_size = in_size;
-    cfg.in = in;
-  } else {
-    if (in_size < sizeof(iv)) return -1;
-    memcpy(iv, in, sizeof(iv));
-    cfg.key = key + SHARED_SECRET_SIZE_HMAC;
-    cfg.in_size = in_size - sizeof(iv);
-    cfg.in = in + sizeof(iv);
+    return aes_crypt(AES_MODE_CBC, AES_OP_DECRYPT, AES_KEY_256, key, iv, in, out, in_size);
   }
-  return block_cipher_dec(&cfg);
+
+  if (in_size < sizeof(iv)) return -1;
+  memcpy(iv, in, sizeof(iv));
+  in_size -= sizeof(iv);
+  in += sizeof(iv);
+  if (out == in - sizeof(iv)) {
+    // HED_AESBlock uses memcpy for distinct input/output pointers.
+    memmove(out, in, in_size);
+    in = out;
+  }
+  return aes_crypt(AES_MODE_CBC, AES_OP_DECRYPT, AES_KEY_256, key + SHARED_SECRET_SIZE_HMAC, iv, in, out, in_size);
 }
 
 bool cp_verify(const uint8_t *key, size_t key_len, const uint8_t *msg, size_t msg_len, const uint8_t *sig,
